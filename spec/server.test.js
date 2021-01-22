24df5d4ab378ba9d88663c37b801dfa68558a323
Paula Herrera <herrera.paula5@gmail.com>
2021-01-21 20:08:10 -0700

    Testing leangth of tests in circleci

--- spec/server.test.js
+++ spec/server.test.js
@@ -6,26 +6,6 @@ beforeAll(() => {
 
 });
 
-describe('GET /activities/:id', () => {
-  test('It should respond with an array', async () => { // not passing
-    const response = await request(app).get('/activities/4');
-    const activities = response.body;
-    expect(Array.isArray(activities)).toBeTruthy();
-    expect(response.statusCode).toBe(200);
-  });
-  test('It should respond with array of activities', async () => {
-    const response = await request(app).get('/activities/4');
-    const activities = response.body;
-    expect(Array.isArray(activities)).toBeTruthy();
-    expect(activities[0]).toHaveProperty('activityId');
-    expect(response.statusCode).toBe(200);
-  });
-  test('It should respond with status 500 if requesting invalid id', async () => {
-    const response = await request(app).get('/activities/abc');
-    expect(response.statusCode).toBe(500);
-  });
-});
-
 describe('GET /homes/:id', () => {
   test('It should respond with an array', async () => { // not passing
     const response = await request(app).get('/homes/4');
@@ -46,7 +26,45 @@ describe('GET /homes/:id', () => {
   });
 });
 
+describe('GET /activities/:id', () => {
+  test('It should respond with an array', async () => { // not passing
+    const response = await request(app).get('/activities/4');
+    const activities = response.body;
+    expect(Array.isArray(activities)).toBeTruthy();
+    expect(response.statusCode).toBe(200);
+  });
+  test('It should respond with array of activities', async () => {
+    const response = await request(app).get('/activities/4');
+    const activities = response.body;
+    expect(Array.isArray(activities)).toBeTruthy();
+    expect(activities[0]).toHaveProperty('activityId');
+    expect(response.statusCode).toBe(200);
+  });
+  test('It should respond with status 500 if requesting invalid id', async () => {
+    const response = await request(app).get('/activities/abc');
+    expect(response.statusCode).toBe(500);
+  });
+});
 
+describe('GET /activities/:id', () => {
+  test('It should respond with an array', async () => { // not passing
+    const response = await request(app).get('/activities/4');
+    const activities = response.body;
+    expect(Array.isArray(activities)).toBeTruthy();
+    expect(response.statusCode).toBe(200);
+  });
+  test('It should respond with array of activities', async () => {
+    const response = await request(app).get('/activities/4');
+    const activities = response.body;
+    expect(Array.isArray(activities)).toBeTruthy();
+    expect(activities[0]).toHaveProperty('activityId');
+    expect(response.statusCode).toBe(200);
+  });
+  test('It should respond with status 500 if requesting invalid id', async () => {
+    const response = await request(app).get('/activities/abc');
+    expect(response.statusCode).toBe(500);
+  });
+});
 
 afterAll((done) => {
   mongoose.connection.close();
